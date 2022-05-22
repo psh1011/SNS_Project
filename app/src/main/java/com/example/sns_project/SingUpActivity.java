66643545ebbb2fab5c7b9ed3d05edcc0ffdd2c9a
박성현 <jjinbbang13@naver.com>
2022-05-22 17:42:48 +0900

    Logcat에서 뜨는 Unable to start activity ComponentInfo만 해결하면 될것같은데 왜 안되는거야

---
+++ app/src/main/java/com/example/sns_project/SingUpActivity.java
@@ -0,0 +1,69 @@
+package com.example.sns_project;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SingUpActivity extends AppCompatActivity {
+    private static final String TAG = "SignUpActivity";
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.signupbutton).setOnClickListener(onClickListener);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+    }
+
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.signupbutton:
+                    signup();
+                    break;
+            }
+        }
+    };
+    private void signup(){
+        String email = ((EditText)findViewById(R.id.email)).getText().toString();
+        String password = ((EditText)findViewById(R.id.password)).getText().toString();
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        }
+                    }
+                });
+    }
+}
